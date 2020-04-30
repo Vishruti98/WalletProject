8ab797ae9b426c4b57245c9a343d02e6a336c913
Vishruti Malviya <vishrutimalviya98@gmail.com>
2020-04-30 23:32:30 +0530

    commit

--- walletBackend/src/test/java/com/project/app/WalletBackendApplicationTests.java
+++ walletBackend/src/test/java/com/project/app/WalletBackendApplicationTests.java
@@ -1,13 +1,109 @@
 package com.project.app;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.project.app.dao.Dao;
+import com.project.app.entities.Wallet;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
 @SpringBootTest
 class WalletBackendApplicationTests {
 
+	
+	@Autowired
+	private Dao dao;
+
 	@Test
-	void contextLoads() {
-	}
+	@Rollback(true)
+	public void testcreateWallet() {
+		Wallet wallet = new Wallet();
+		wallet.setUsername("Vishruti");
+		wallet.setPassword("Vishruti@123");
+		wallet.setPhoneNumber("8765376287");
+
+		int account = dao.createUser(wallet);
+		System.out.println("Created");
+		System.out.println(wallet.getBalance());
+		Wallet sample = dao.getUserById(account);
+		System.out.println(sample.getBalance());
+		assertThat(wallet.getUsername()).isEqualTo(sample.getUsername());	
+		assertThat(wallet.getPassword()).isEqualTo(sample.getPassword());
+		assertThat(wallet.getBalance()).isEqualTo(sample.getBalance());
+		}
+	
+	@Test
+	@Rollback(true)
+	public void testgetUserById() {
+		Wallet wallet = dao.getUserById(10034);
+		System.out.println(wallet.getUsername());
+		System.out.println(wallet.getPassword());
+		assertThat(wallet.getUsername()).isEqualTo("Ram");
+		assertThat(wallet.getPassword()).isEqualTo("JaiShriRam@1223");
+		//assertThat(wallet.getBalance()).isEqualTo(sample.getBalance());
+		}
+	
+	@Test
+	@Rollback(true)
+	public void testDepositAndWithdrawMoney() {
+		Wallet wallet = new Wallet();
+		wallet.setUsername("Vishruti");
+		wallet.setPassword("Vishruti@123");
+		wallet.setPhoneNumber("8765376287");
+
+		int account = dao.createUser(wallet);
+		Wallet retrieve = dao.getUserById(account);
+		
+		double amount=200;
+		dao.deposit(account, amount);
+		assertThat(wallet.getBalance()).isEqualTo(retrieve.getBalance());
+		assertThat(retrieve.getBalance()).isEqualTo(200);
+		
+		double withdrawAmount=20;
+		dao.withdraw(account, withdrawAmount);
+		assertThat(wallet.getBalance()).isEqualTo(retrieve.getBalance());
+		assertThat(retrieve.getBalance()).isEqualTo(180);
+		}
+	
+	@Test
+	@Rollback(true)
+	public void testTransferMoney() {
+		Wallet wallet = new Wallet();
+		wallet.setUsername("Vishruti");
+		wallet.setPassword("Vishruti@123");
+		wallet.setPhoneNumber("8765376287");
+
+		int account = dao.createUser(wallet);
+		Wallet retrieve = dao.getUserById(account);
+		
+		double amount=200;
+		dao.deposit(account, amount);
+		
+		
+		Wallet wallet1 = new Wallet();
+		wallet1.setUsername("Vallari");
+		wallet1.setPassword("Vallari@123");
+		wallet1.setPhoneNumber("7557372063");
+
+		int account1 = dao.createUser(wallet1);
+		Wallet retrieve1 = dao.getUserById(account1);
+		
+		double amount1=500;
+		dao.deposit(account1, amount1);
+		
+		dao.transfer(100, account1, account);
+		assertThat(retrieve.getBalance()).isEqualTo(100);
+		assertThat(retrieve1.getBalance()).isEqualTo(600);
+		}
+
 
 }
